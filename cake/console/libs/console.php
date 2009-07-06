apoorvnag/Slack-TODO

commit 448d331355c9c891d99bfcacae343b1b83a82079

Added in Model->save() method along with help text explaining how to use it
git-svn-id: https://svn.cakephp.org/repo/branches/1.2.x.x@5219 3807eeeb-6ff5-0310-8944-8be069107fe0

cake/console/libs/console.php
@@ -70,6 +70,11 @@ class ConsoleShell extends Shell {
 					$this->out('To dynamically remove associations, you can do the following:');
 					$this->out("\t ModelA unbind <association> ModelB");
 					$this->out("where the supported associations are the same as above");
+					$this->out('');
+					$this->out("To save a new field in a model, you can do the following:");
+					$this->out("\tModelA->save(array('foo' => 'bar', 'baz' => 0))");
+					$this->out("where you are passing a hash of data to be saved in the format");
+					$this->out("of field => value pairs");
 				break;
 				case 'quit':
 				case 'exit':
@@ -130,7 +135,6 @@ class ConsoleShell extends Shell {
 					// Remove any bad info
 					$command = strip_tags($command);
 					$command = str_replace($this->badCommandChars, "", $command);
-					$command = str_replace(";", "", $command);
 
 					// Do we have a valid model?
 					list($modelToCheck, $tmp) = explode('->', $command);
@@ -180,8 +184,24 @@ class ConsoleShell extends Shell {
 						$this->out("$modelToCheck is not a valid model");
 					}
 
-					break;
+				break;
+				case (strpos($command, '->save') > 0):
+					// Validate the model we're trying to save here
+					$command = strip_tags($command);
+					$command = str_replace($this->badCommandChars, "", $command);
+					list($modelToSave, $tmp) = explode("->", $command);
 					
+					if ($this->isValidModel($modelToSave)) {
+						// Extract the array of data we are trying to build
+						list($foo, $data) = explode("->save", $command);
+						$badChars = array("(", ")");
+						$data = str_replace($badChars, "", $data);
+						$saveCommand = "\$this->{$modelToSave}->save(array('{$modelToSave}' => array({$data})));";
+						$this->out($saveCommand);
+						@eval($saveCommand);
+					}
+
+				break;				
 				default:
 					$this->out("Invalid command\n");
 				break;

commit 3f0e0b7c9dacb14f75b5206ab93abb31df4309ec

Fixing bug introduced in r5219 that was preventing model save operations from working in Console shell
git-svn-id: https://svn.cakephp.org/repo/branches/1.2.x.x@8222 3807eeeb-6ff5-0310-8944-8be069107fe0

cake/console/libs/console.php
@@ -252,8 +252,7 @@ class ConsoleShell extends Shell {
 					if ($this->__isValidModel($modelToSave)) {
 						// Extract the array of data we are trying to build
 						list($foo, $data) = explode("->save", $command);
-						$badChars = array("(", ")");
-						$data = str_replace($badChars, "", $data);
+						$data = preg_replace('/^\(*(array)?\(*(.+?)\)*$/i', '\\2', $data);
 						$saveCommand = "\$this->{$modelToSave}->save(array('{$modelToSave}' => array({$data})));";
 						@eval($saveCommand);
 						$this->out('Saved record for ' . $modelToSave);