kangaroot/cakephp1x

Give PaginatorHelper's next/prev links the correct 'rel' attribute
It's a good idea to give links such as next/prev the 'rel' attribute.
See the following pages for more information:
http://www.w3.org/TR/html4/struct/links.html#edef-A
http://www.w3.org/TR/html4/types.html#type-links

cake/libs/view/helpers/paginator.php
@@ -256,6 +256,10 @@ class PaginatorHelper extends AppHelper {
  * @access public
  */
 	function prev($title = '<< Previous', $options = array(), $disabledTitle = null, $disabledOptions = array()) {
+		$_defaults = array(
+			'rel' => 'prev'
+		);
+		$options = array_merge($_defaults, (array)$options);
 		return $this->__pagingLink('Prev', $title, $options, $disabledTitle, $disabledOptions);
 	}
 
@@ -276,6 +280,10 @@ class PaginatorHelper extends AppHelper {
  * @access public
  */
 	function next($title = 'Next >>', $options = array(), $disabledTitle = null, $disabledOptions = array()) {
+		$_defaults = array(
+			'rel' => 'next'
+		);
+		$options = array_merge($_defaults, (array)$options);
 		return $this->__pagingLink('Next', $title, $options, $disabledTitle, $disabledOptions);
 	}
 

apoorvnag/Slack-TODO

Signed-off-by: mark_story <mark@mark-story.com>

cake/libs/view/helpers/paginator.php
@@ -252,6 +252,10 @@ class PaginatorHelper extends AppHelper {
  * @return string A "previous" link or $disabledTitle text if the link is disabled.
  */
 	public function prev($title = '<< Previous', $options = array(), $disabledTitle = null, $disabledOptions = array()) {
+		$defaults = array(
+			'rel' => 'prev'
+		);
+		$options = array_merge($defaults, (array)$options);
 		return $this->__pagingLink('Prev', $title, $options, $disabledTitle, $disabledOptions);
 	}
 
@@ -271,6 +275,10 @@ class PaginatorHelper extends AppHelper {
  * @return string A "next" link or or $disabledTitle text if the link is disabled.
  */
 	public function next($title = 'Next >>', $options = array(), $disabledTitle = null, $disabledOptions = array()) {
+		$defaults = array(
+			'rel' => 'next'
+		);
+		$options = array_merge($defaults, (array)$options);
 		return $this->__pagingLink('Next', $title, $options, $disabledTitle, $disabledOptions);
 	}
 