cakephp/cakephp

Adds two new time testing methods: 'isFuture' and 'isPast'
TestCases included

lib/Cake/Utility/CakeTime.php
@@ -465,6 +465,32 @@ class CakeTime {
 		$timestamp = self::fromString($dateString, $timezone);
 		return date('Y-m-d', $timestamp) == date('Y-m-d', time());
 	}
+                
+/**
+ * Returns true if given datetime string is in the future.
+ *
+ * @param integer|string|DateTime $dateString UNIX timestamp, strtotime() valid string or DateTime object
+ * @param string|DateTimeZone $timezone Timezone string or DateTimeZone object
+ * @return boolean True if datetime string is in the future
+ * @link http://book.cakephp.org/2.0/en/core-libraries/helpers/time.html#testing-time
+ */
+	public static function isFuture($dateString, $timezone = null) {
+		$date = self::fromString($dateString, $timezone);
+		return date('Y-m-d hh:mm:ss', $date) < date('Y-m-d hh:mm:ss', time());
+	}
+        
+/**
+ * Returns true if given datetime string is in the past.
+ *
+ * @param integer|string|DateTime $dateString UNIX timestamp, strtotime() valid string or DateTime object
+ * @param string|DateTimeZone $timezone Timezone string or DateTimeZone object
+ * @return boolean True if datetime string is in the past
+ * @link http://book.cakephp.org/2.0/en/core-libraries/helpers/time.html#testing-time
+ */
+	public static function isPast($dateString, $timezone = null) {
+		$date = self::fromString($dateString, $timezone);
+		return date('Y-m-d hh:mm:ss', $date) > date('Y-m-d hh:mm:ss', time());
+	}
 
 /**
  * Returns true if given datetime string is within this week.

apoorvnag/Slack-TODO

lib/Cake/Utility/CakeTime.php
@@ -465,6 +465,32 @@ class CakeTime {
 		$timestamp = self::fromString($dateString, $timezone);
 		return date('Y-m-d', $timestamp) == date('Y-m-d', time());
 	}
+                
+/**
+ * Returns true if given datetime string is in the future.
+ *
+ * @param integer|string|DateTime $dateString UNIX timestamp, strtotime() valid string or DateTime object
+ * @param string|DateTimeZone $timezone Timezone string or DateTimeZone object
+ * @return boolean True if datetime string is in the future
+ * @link http://book.cakephp.org/2.0/en/core-libraries/helpers/time.html#testing-time
+ */
+	public static function isFuture($dateString, $timezone = null) {
+		$date = self::fromString($dateString, $timezone);
+		return date('Y-m-d H:i:s', $date) < date('Y-m-d H:i:s', time());
+	}
+        
+/**
+ * Returns true if given datetime string is in the past.
+ *
+ * @param integer|string|DateTime $dateString UNIX timestamp, strtotime() valid string or DateTime object
+ * @param string|DateTimeZone $timezone Timezone string or DateTimeZone object
+ * @return boolean True if datetime string is in the past
+ * @link http://book.cakephp.org/2.0/en/core-libraries/helpers/time.html#testing-time
+ */
+	public static function isPast($dateString, $timezone = null) {
+		$date = self::fromString($dateString, $timezone);
+		return date('Y-m-d H:i:s', $date) > date('Y-m-d H:i:s', time());
+	}
 
 /**
  * Returns true if given datetime string is within this week.